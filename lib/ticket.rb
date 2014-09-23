0e377f43aabed5353b33a2b9b71aec08e57e5ff6
Joachim Törnqvist <Joachim.tornqvist@elev.it-gymnasiet.se>
2014-09-23 15:14:12 +0200

    Lektion 1
    Frågar efter tal, matar in tal, ger ut samma.

---
+++ lib/ticket.rb
@@ -0,0 +1,15 @@
+def ask_age
+  puts "Enter your age"
+  age = gets.to_i
+  return age
+end
+
+
+def ticket_price(age)
+  #if ask_age = < 18
+   # puts "10kr"
+  #elsif ask_age = >65
+   # puts "15kr"
+  #7end
+end
+puts ask_age()
